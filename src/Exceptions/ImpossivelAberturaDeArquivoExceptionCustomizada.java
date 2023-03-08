7738f0f2fe9efdcebe091d0a5ac7163367da3b69
Vitor Brussolo Zerbato <70340981+CadisRaziel@users.noreply.github.com>
2023-03-08 15:18:18 -0300

    Create ImpossivelAberturaDeArquivoExceptionCustomizada.java

---
+++ src/Exceptions/ImpossivelAberturaDeArquivoExceptionCustomizada.java
@@ -0,0 +1,31 @@
+//Criando classe para tratamento de exceção personalizado
+
+package Exceptions;
+
+import java.io.*;
+
+public class ImpossivelAberturaDeArquivoExceptionCustomizada extends Exception {
+     private String nomeDoArquivo;
+     private String diretorio;
+
+
+     public ImpossivelAberturaDeArquivoExceptionCustomizada ( String nomeDoArquivo, String diretorio) {
+     	super("O arquivo " + nomeDoArquivo + "Não foi encontrado no diretorio" + diretorio);
+     	this.nomeDoArquivo = nomeDoArquivo;
+     	this.diretorio = diretorio;
+
+     }
+    
+
+     @Override
+    public String toString() {
+        return "ImpossivelAberturaDeArquivoExceptionCustomizada{" +
+                "nomeDoArquivo='" + nomeDoArquivo + '\'' +
+                ", diretorio=" + diretorio + '\'' +
+                '}';
+    }
+
+    //posso fazer um Override do getCause... de tudo que uma exception tem
+
+}
+
